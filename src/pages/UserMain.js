9eca45eb5f6afc61f0e011e3a84214c80c15b302
JDH <yjkl0516@gmail.com>
2024-09-26 18:37:50 +0900

    .

--- src/pages/UserMain.js
+++ src/pages/UserMain.js
@@ -5,25 +5,6 @@ import { useDispatch, useSelector } from "react-redux";
 
 function UserMain() {
 
-    const testHandler = () => {
-        
-        const userInfoString = localStorage.getItem('userInfo');
-
-        // userInfo가 null이 아닌 경우에만 파싱
-        if (userInfoString) {
-            // JSON 문자열을 객체로 변환
-            const userInfo = JSON.parse(userInfoString);
-            console.log(userInfo); // 객체 출력
-
-            // userId 접근
-            const userId = userInfo.userId; // 객체의 userId 프로퍼티에 접근
-            console.log(userId);
-        } else {
-            console.log('userInfo가 존재하지 않습니다.');
-        }
-        
-    }
-
     return(
         <>
         <div className="user-main-container">
@@ -46,8 +27,6 @@ function UserMain() {
                         <span>평균 진료비 보기</span>
                     </NavLink>
                 </li>
-                
-                <button onClick={testHandler}>test</button>
             </ul>
         </div>
         <div className="footer-container">
