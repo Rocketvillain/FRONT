20651b37316c4b00a12c9e88b5caa0ea17362abd
JDH <yjkl0516@gmail.com>
2024-09-27 16:05:34 +0900

    [Feat] - redux-persist 라이브러리 추가 =>  새로고침해도 redux state가 유지될 수 있도록 한다!!!

--- src/Store.js
+++ src/Store.js
@@ -1,12 +1,22 @@
 import rootReducer from './modules';
 import { composeWithDevTools } from '@redux-devtools/extension';
+import { persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
 import { createStore } from 'redux';
 import { applyMiddleware } from 'redux';
 import {thunk} from 'redux-thunk';
 import logger from 'redux-logger';
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['user', 'hospital']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-    rootReducer,
+    persistedReducer,
     composeWithDevTools(applyMiddleware(thunk, logger))
 );
 
