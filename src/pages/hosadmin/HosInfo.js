8aaa7c6fe942dc7278d553644b685c86a4c1266a
dmsgpk237 <a01027947353@gmail.com>
2024-10-01 10:18:10 +0900

    [FEAT] 병원관리자 마이페이지 병원 api 연결

--- src/pages/hosadmin/HosInfo.js
+++ src/pages/hosadmin/HosInfo.js
@@ -1,149 +1,141 @@
 // 병원관리자 마이페이지(병원정보 조회, 수정)
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from 'react-redux';
+import { useNavigate } from "react-router-dom";
 import '../../css/HosInfo.css';
+
 function HosInfo() {
 
-        const [hosInfo, setHosInfo] = useState({
-            userId: 'user01',
-            hospitalName: '강아지 동물병원',
-            address: '서울 구로구 내마음대로 123-24',
-            ceoName: '지동동구리',
-            businessNumber: '125-45-10882',
-            description: '병원 소개 글을 작성하는 곳입니다.',
-        });
-        const [previewImage, setPreviewImage] = useState({
-            ceoImage: null,
-            hospitalImage: null,
-        });
-    
-        // 파일 업로드 핸들러
-        const handleImageChange = (e, imageType) => {
-            if (e.target.files && e.target.files[0]) {
-                const file = e.target.files[0];
-                // 미리보기 이미지 설정
-                setPreviewImage((prevImages) => ({
-                    ...prevImages,
-                    [imageType]: URL.createObjectURL(file),
-                }));
+    const [reserTerm, setReserTerm] = useState("");
+    const [hospitals, setHospitals] = useState([]);
+    const [filteredHospitals, setFilteredHospitals] = useState([]);
+    const navigate = useNavigate();
+
+    const hospital = useSelector(state => state.hospital.hospital); 
+
+    // 병원 정보를 API에서 받아오기
+    useEffect(() => {
+        fetch("http://localhost:8080/api/v1/hospital") // 백엔드 API 엔드포인트
+        .then((response) => response.json())
+        .then((data) => {
+            if (Array.isArray(data)) {
+            setHospitals(data);
+            setFilteredHospitals(data);
+            } else {
+            console.error("Received data is not an array:", data);
+            setHospitals([]);
             }
-        };
+        })
+        .catch((error) => console.error("Error fetching hospitals:", error));
+    }, []);
     
-        return (
-            <div className="hos-info-container">
-                <div className="hos-info-content">
-                    <div className="hos-info-header">
-                        <h2>병원 정보</h2>
-                        <img src="/images/logo2.png" alt="Healing Pets Logo" className="hos-logo" />
+    return (
+        <div className="hos-info-container">
+            <div className="hos-info-content">
+                <div className="hos-info-header">
+                    <h2>병원 정보</h2>
+                    <img src={`/${hospital.ownerImage}`} alt="Healing Pets Logo" className="hos-logo" />
+                </div>
+                <table className="hos-info-table">
+                    <tbody>
+                        <tr>
+                            <th>아이디</th>
+                            <td>{hospital.userId}</td>
+                        </tr>
+                        <tr>
+                            <th>병원 이름</th>
+                            <td>
+                                <input
+                                    type="text"
+                                    value={hospital.name}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>주소</th>
+                            <td>
+                                <input
+                                    type="text"
+                                    value={hospital.address}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>대표자 이름</th>
+                            <td>
+                                <input
+                                    type="text"
+                                    value={hospital.ownerName}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>사업자 등록 번호</th>
+                            <td>
+                                <input
+                                    type="text"
+                                    value={hospital.businessNo}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>병원소개</th>
+                            <td>
+                                <textarea
+                                    value={hospital.info}
+                                ></textarea>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div className="image-upload-section">
+                    <div className="image-upload">
+                        <img
+                            src={`/${hospital.ownerImage}`}
+                            alt="대표자 이미지"
+                            className="image-preview"
+                        />
+                        <input
+                            type="file"
+                            id="ceoImageUpload"
+                            accept="image/*"
+                            style={{ display: 'none' }}
+                        />
+                        <button
+                            type="button"
+                            className="image-upload-button"
+                        >
+                            대표자 이미지 등록
+                        </button>
                     </div>
-                    <table className="hos-info-table">
-                        <tbody>
-                            <tr>
-                                <th>아이디</th>
-                                <td>{hosInfo.userId}</td>
-                            </tr>
-                            <tr>
-                                <th>병원 이름</th>
-                                <td>
-                                    <input
-                                        type="text"
-                                        value={hosInfo.hospitalName}
-                                        onChange={(e) => setHosInfo({ ...hosInfo, hospitalName: e.target.value })}
-                                    />
-                                </td>
-                            </tr>
-                            <tr>
-                                <th>주소</th>
-                                <td>
-                                    <input
-                                        type="text"
-                                        value={hosInfo.address}
-                                        onChange={(e) => setHosInfo({ ...hosInfo, address: e.target.value })}
-                                    />
-                                </td>
-                            </tr>
-                            <tr>
-                                <th>대표자 이름</th>
-                                <td>
-                                    <input
-                                        type="text"
-                                        value={hosInfo.ceoName}
-                                        onChange={(e) => setHosInfo({ ...hosInfo, ceoName: e.target.value })}
-                                    />
-                                </td>
-                            </tr>
-                            <tr>
-                                <th>사업자 등록 번호</th>
-                                <td>
-                                    <input
-                                        type="text"
-                                        value={hosInfo.businessNumber}
-                                        onChange={(e) => setHosInfo({ ...hosInfo, businessNumber: e.target.value })}
-                                    />
-                                </td>
-                            </tr>
-                            <tr>
-                                <th>병원소개</th>
-                                <td>
-                                    <textarea
-                                        value={hosInfo.description}
-                                        onChange={(e) => setHosInfo({ ...hosInfo, description: e.target.value })}
-                                    ></textarea>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <div className="image-upload-section">
-                        <div className="image-upload">
-                            <img
-                                src={previewImage.ceoImage || '/images/owner.png'}
-                                alt="대표자 이미지"
-                                className="image-preview"
-                            />
-                            <input
-                                type="file"
-                                id="ceoImageUpload"
-                                accept="image/*"
-                                onChange={(e) => handleImageChange(e, 'ceoImage')}
-                                style={{ display: 'none' }}
-                            />
-                            <button
-                                type="button"
-                                className="image-upload-button"
-                                onClick={() => document.getElementById('ceoImageUpload').click()}
-                            >
-                                대표자 이미지 등록
-                            </button>
-                        </div>
-                        <div className="image-upload">
-                            <img
-                                src={previewImage.hospitalImage || '/images/hospital.jpg'}
-                                alt="병원 이미지"
-                                className="image-preview"
-                            />
-                            <input
-                                type="file"
-                                id="hospitalImageUpload"
-                                accept="image/*"
-                                onChange={(e) => handleImageChange(e, 'hospitalImage')}
-                                style={{ display: 'none' }}
-                            />
-                            <button
-                                type="button"
-                                className="image-upload-button"
-                                onClick={() => document.getElementById('hospitalImageUpload').click()}
-                            >
-                                병원 이미지 등록
-                            </button>
-                        </div>
-                    </div>
-                    <div className="submit-button-container">
-                        <button type="button" className="submit-button">
-                            확인
+                    <div className="image-upload">
+                        <img
+                            src={`/${hospital.infoImage}`}
+                            alt="병원 이미지"
+                            className="image-preview"
+                        />
+                        <input
+                            type="file"
+                            id="hospitalImageUpload"
+                            accept="image/*"
+                            style={{ display: 'none' }}
+                        />
+                        <button
+                            type="button"
+                            className="image-upload-button"
+                        >
+                            병원 이미지 등록
                         </button>
                     </div>
                 </div>
+                <div className="submit-button-container">
+                    <button type="button" className="submit-button">
+                        확인
+                    </button>
+                </div>
             </div>
-        );
+        </div>
+    );
 }
 
 export default HosInfo;
